79717a3a57002418fb871537d6ff1d2a244d3293
ivanparradogarcia <ivanparradogarcia@gmail.com>
2025-11-13 10:21:56 +0100

    minimum margin fixes

--- paniagua_sale_minimum_margin/models/product_template.py
+++ paniagua_sale_minimum_margin/models/product_template.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -11,4 +11,24 @@ class ProductTemplate(models.Model):
         default=0.0,
         help='Porcentaje de margen mínimo permitido para vender este producto',
     )
+
+    current_margin = fields.Float(
+        string='Margen Actual (%)',
+        compute='_compute_current_margin',
+        store=False,
+        help='Margen de beneficio actual: ((Precio Venta - Coste) / Precio Venta) * 100',
+    )
+
+    @api.depends('list_price', 'standard_price')
+    def _compute_current_margin(self):
+        """Calcula el margen actual basado en precio de venta y coste"""
+        for product in self:
+            if product.list_price > 0:
+                # Margen = ((Precio Venta - Coste) / Precio Venta) * 100
+                margin = ((product.list_price - product.standard_price) / product.list_price) * 100
+                product.current_margin = margin
+            else:
+                product.current_margin = 0.0
+
+
 
